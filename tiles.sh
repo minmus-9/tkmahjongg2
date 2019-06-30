7e63e0e9e0f2baa5c238ef815099af9192a1688b
cave canem <cave-canem@waggly.dog>
2019-06-29 17:28:21 -0700

    just need to get tile labels larger...

--- tiles.sh
+++ tiles.sh
@@ -1,6 +1,40 @@
 #!/bin/sh
 
-find tiles/raw -name '*.png' -print | sort | (
+rm -rf tiles/cooked
+mkdir -p tiles/cooked
+cd tiles/cooked
+find ../raw -name '*.png' -print | sort | (
+    while read f; do
+        g=`basename $f`
+        lbl="X"
+        geom="+30+20"
+        case "$g" in
+            *-[0-9].png) continue;;
+            face-down*) continue;;
+            bamboo*|man*|pin*) lbl=`echo $g | tr -cd 0-9`;;
+            dragon-chun*) lbl="C";;
+            dragon-green*) lbl="G";;
+            dragon-haku*) lbl="H";;
+            flower-bamboo*) lbl="Bamboo"; geom=+0+16;;
+            flower-chrys*) lbl="Chrysanthemum"; geom=+0+16;;
+            flower-orchid*) lbl="Orchid"; geom=+0+16;;
+            flower-plum*) lbl="Plum"; geom=+0+16;;
+            season-spring*) lbl="Spring"; geom=+0+16;;
+            season-summer*) lbl="Summer"; geom=+0+16;;
+            season-autumn*) lbl="Autumn"; geom=+0+16;;
+            season-winter*) lbl="Winter"; geom=+0+16;;
+            wind-north*) lbl=N;;
+            wind-east*) lbl=E;;
+            wind-south*) lbl=S;;
+            wind-west*) lbl=W;;
+        esac
+        convert $f -gravity north -annotate $geom $lbl -fill black \
+            -font Times -pointsize 48 \
+            $g
+    done
+)
+
+find . -name '*.png' -print | sort | (
     echo "########################################################################"
     echo "## tiles.tcl"
     echo
